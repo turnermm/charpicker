f21fa538787777d27def217fee18a21dc25ca40b
Robert Bogenschneider <bogi@uea.org>
2013-10-21 22:15:15 +0200

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['chars']                 = 'Komo-disigita listo de literoj aldonendaj al la selektilo de Specialaj Literoj';
+$lang['del_chars']             = 'Komo-disigita listo de literoj forigendaj el la selektilo de Specialaj Literoj';
