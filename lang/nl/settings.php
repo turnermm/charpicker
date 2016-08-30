06b844fbf6f0e828956a6f60fc66dce20be4a45f
Gerrit Uitslag <klapinklapin@gmail.com>
2016-08-30 08:55:18 +0200

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -2,8 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Rene <wllywlnt@yahoo.com>
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
  */
-$lang['chars']                 = 'Een comma gescheiden lijst van karakters die worden toegevoegd aan de Special Characters picker';
-$lang['del_chars']             = 'Een comma gescheiden lijst van karakters die worden verwijderd van de Special Characters picker';
+$lang['chars']                 = 'Een komma gescheiden lijst van tekens die worden toegevoegd aan de Speciale Tekens keuzemenu';
+$lang['del_chars']             = 'Een komma gescheiden lijst van tekens die worden verwijderd uit de Speciale Tekens keuzemenu';
+$lang['del_all']               = 'Verwijder alle tekens uitgezonderd de lijst van toegevoegde tekens; maakt het mogelijk om de hele set met speciale tekens zelf te defineren.';
