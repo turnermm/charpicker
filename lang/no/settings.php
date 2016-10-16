fc3d24e1feb023b4e925233a5dab07651ecc39cc
Arne Hanssen <arne.hanssen@getmail.no>
2016-10-16 15:10:10 +0200

    translation update

---
+++ lang/no/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Arne Hanssen <arne.hanssen@getmail.no>
+ */
+$lang['chars']                 = 'Kommaseparert liste med tegn som vi bli lagt til spesialtegnvelgeren';
+$lang['del_chars']             = 'Kommaseparert liste med tegn som vi bli slettet fra spesialtegnvelgeren';
+$lang['del_all']               = 'Slett alle andre tegn en de fra listen med tegn som er lagt til; muliggjør en helt brukerdefinert mengde med spesialtegn.';
